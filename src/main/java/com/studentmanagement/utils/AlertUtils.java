c5053cfd2b8eae8b2812746c9a674ccfa8a193e5
christine chemali <christine.chemali@laplateforme.io>
2025-07-04 07:35:46 +0200

    feat:Add showWarning methode to AlertUtils for warning alert - Implement showWarning method in AlertUtils for warning alerts. - This method creates an Alert of type WARNING, enabling customizable titles and messages. - Enhance AlertUtils functionnality by providing a consistent way to handle warning notifications.

--- src/main/java/com/studentmanagement/utils/AlertUtils.java
+++ src/main/java/com/studentmanagement/utils/AlertUtils.java
@@ -2,6 +2,7 @@ package com.studentmanagement.utils;
 
 import javafx.scene.control.Alert;
 
+//Information alert message
 public class AlertUtils {
     public static void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -11,8 +12,18 @@ public class AlertUtils {
         alert.showAndWait();
     }
 
+//Error alert message   
 public static void showError(String  title, String message) {
     Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setTitle(title);
+    alert.setHeaderText(null);
+    alert.setContentText(message);
+    alert.showAndWait();
+}
+
+//Warning alert message
+public static void showWarning(String title, String message){
+    Alert alert = new Alert(Alert.AlertType.WARNING);
     alert.setTitle(title);
     alert.setHeaderText(null);
     alert.setContentText(message);
