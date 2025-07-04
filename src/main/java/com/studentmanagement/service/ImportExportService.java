ed59cbe24216fb0f878045d9f31eddd1fca1df32
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-07-04 14:52:38 +0200

    feat: created and tested csvHandler and importExportService
    can now parse student info from student objects

--- src/main/java/com/studentmanagement/service/ImportExportService.java
+++ src/main/java/com/studentmanagement/service/ImportExportService.java
@@ -1,19 +1,38 @@
 package com.studentmanagement.service;
 
+import java.util.*;
+import java.io.*;
+//import com.opencsv.*;
+
+import com.studentmanagement.utils.CSVHandler;
+import com.studentmanagement.model.Student;
+
+
 class ImportExportService{
-    CsvHandler csvHandler= new CsvHandler();
+    CSVHandler csvHandler= new CSVHandler();
 
-    PdfExporter pdfExporter=new PdfExporter();
 
-    public ImportExportService(){
+    public  ImportExportService(){
 
     }
+
+
 
     void exportToCSV(List<Student> studentList){
+        try(PrintWriter writer= new PrintWriter(new FileWriter("export.csv"))) {
+            writer.println("id, first_name, last_name, age, grade");
+
+            for (Student student : studentList) {
+                String line = csvHandler.formatStudentToCSV(student);
+                writer.println(line);
+            }
+
+            System.out.println("exported to export.csv successfully.");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 
-    void exportToPdf(){
 
-    }
 }
---
+++ src/test/java/com/studentmanagement/utils/CSVHandlerTest.java
@@ -0,0 +1,34 @@
+package com.studentmanagement.utils;
+
+import com.studentmanagement.model.Student;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CSVHandlerTest {
+
+    @Test
+    public void testFormatStudentToCSV(){
+        CSVHandler handler=new CSVHandler();
+        Student testStudent= new Student(1, "bob", "Builder", 5, 93.6);
+
+        String expected= "1,bob,Builder,5,93.60";
+        String actual= handler.formatStudentToCSV(testStudent);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseStudentFromCSVLine(){
+        CSVHandler handler = new CSVHandler();
+        String line = "1,bob,Builder,5,93.6";
+
+        Student s= handler.parsestudentFromCSVLine(line);
+
+        assertEquals(1, s.getId());
+        assertEquals("bob", s.getFirstName());
+        assertEquals("Builder", s.getLastName());
+        assertEquals(5, s.getAge());
+        assertEquals(93.60, s.getGrade());
+    }
+}
