4b98ef5d873e5cd2fbd10c2b1c92a904ecb8f9b3
christine chemali <christine.chemali@laplateforme.io>
2025-07-04 11:50:03 +0200

    feat : Implement color variables in CSS for JavaFX - Added color variables to Css to standardize and simplify color management across the application - Improved maintanability by using variables for comonly used colors, facilitating future updates - Enhanced visual consistancy of the application by ensuring uniform color usage

--- src/main/java/com/studentmanagement/Main.java
+++ src/main/java/com/studentmanagement/Main.java
@@ -11,9 +11,13 @@ public class Main extends Application {
    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // load tab.fxml
+
+        // load login view
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        
+        // Comment up and uncomment down to have the main application tab view
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/tab.fxml"));
-       
+
         Image icon = new Image(getClass().getResourceAsStream("/images/icon.png"));
         primaryStage.getIcons().add(icon);
 
