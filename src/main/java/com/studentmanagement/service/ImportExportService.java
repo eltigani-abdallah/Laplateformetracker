9cfe0176e33d3e9f5daf7cb88497b83fb34a7639
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-07-04 14:52:38 +0200

    feat: created and tested csvHandler and importExportService
    can now parse student info from student objects

--- src/main/java/com/studentmanagement/service/ImportExportService.java
+++ src/main/java/com/studentmanagement/service/ImportExportService.java
@@ -20,7 +20,25 @@ class ImportExportService{
 
 
 
+
+
     void exportToCSV(List<Student> studentList){
+        try(PrintWriter writer= new PrintWriter(new FileWriter("export.csv"))) {
+            writer.println("id, first_name, last_name, age, grade");
+
+            for (Student student : studentList) {
+                String line = csvHandler.formatStudentToCSV(student);
+                writer.println(line);
+            }
+
+            System.out.println("exported to export.csv successfully.");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
         try(PrintWriter writer= new PrintWriter(new FileWriter("export.csv"))) {
             writer.println("id, first_name, last_name, age, grade");
 
