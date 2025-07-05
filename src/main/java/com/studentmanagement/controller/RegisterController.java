49547fb602451b0093a44926aa63b2de38a3f6be
christine chemali <christine.chemali@laplateforme.io>
2025-07-05 22:14:40 +0200

    feat: Implement registration logic in RegisterConroller - Added handlRegister method to process user Registration including : - Validation for required fields, ensuring no fields are empty - Confirmation check for matching passwords - Password strenght validation with criteria for lenght, uppercase letters, lowercase letters, and special characters - User registration functionnality using the AuthenticationService - Success and error handling with user feedback through AlertUtils - Implemented clearForm method to reset input fields after registration

--- src/main/java/com/studentmanagement/controller/RegisterController.java
+++ src/main/java/com/studentmanagement/controller/RegisterController.java
@@ -1,6 +1,8 @@
 package com.studentmanagement.controller;
 
 import com.studentmanagement.service.AuthenticationService;
+import com.studentmanagement.utils.AlertUtils;
+import com.studentmanagement.model.User;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -45,7 +47,66 @@ public class RegisterController {
     //Handles the registration of a new user
     @FXML
     private void handleRegister(){
+        String username = textFieldUsername.getText().trim();
+        String password = textFieldPassword.getText().trim();
+        String confirmPassword = textFieldConfirmPassword.getText().trim();
+        
+        //Required fields validation
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+            AlertUtils.showError("Erreur", "Remplis tous les champs,\nil en manque sûrement un !");
+            return;
+        }
 
+        //Check that password match
+        if (!password.equals(confirmPassword)){
+            AlertUtils.showError("Erreur", "Regarde bien tes mots de passe !\nIls ne sont pas identiques.");
+            textFieldPassword.clear();
+            textFieldConfirmPassword.clear();
+            textFieldUsername.requestFocus();
+            return;
+        }
+
+        //Password validation
+        boolean isValid = true;
+        StringBuilder errorMessage = new StringBuilder("Ton mot de passe doit respecter ces critères :");
+        if(password.length()<8) {
+            errorMessage.append("\n- Au moins 8 caractères");
+            isValid = false;
+        }
+        if (!password.matches(".*[A-Z].*")){
+            errorMessage.append("\n- Au moins une lettre majuscule");
+            isValid = false;
+        }
+        if (!password.matches(".*[a-z]")){
+            errorMessage.append("\n- Au moins une lettre minuscule");
+            isValid = false;
+        }
+        if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*")){
+            errorMessage.append("\n- Au moins un caractère spécial (!@#$%^&* etc.)");
+            isValid = false;
+        }
+        if (!isValid){
+            AlertUtils.showError("Erreur", "Oups ! Mot de passe trop simple\n"  + errorMessage.toString());
+            return;
+        }
+        try{
+            //Create a new user
+            User newUser = new User(username, password);
+
+            //Use the register method of the service
+            authService.register(newUser);
+
+            //Registration success
+            AlertUtils.showInformation("Succes", "Super !\nTu es bien enregisté,\nmaintenant tu peux te connecter");
+            clearForm();
+            showLoginView();
+        }  catch (RuntimeException e) {
+            AlertUtils.showInformation("Information", "Ce nom d'utilisateur existe peut-être déja.\nVérifie tes informations.");
+            textFieldUsername.requestFocus();
+        } catch (Exception e){
+            AlertUtils.showError("Erreur", "Aïe !\nUn petit problème technique est survenu.\nNe t'inquiète pas, nos équipes sont sur le coup !");
+            clearForm();
+        }
     }
 
     //Handles the return to the login page
@@ -53,7 +114,14 @@ public class RegisterController {
     private void handleBackToLogin(){
 
     }
-    
+
+    //Clears the form fields
+    private void clearForm(){
+        textFieldUsername.clear();
+        textFieldPassword.clear();
+        textFieldConfirmPassword.clear();
+    }
+
     //Setter for dependency injection
     public void setAuthService(AuthenticationService authService) {
         this.authService = authService;
